55248b77a85485f213c3e32fbb7503f4dd9cf3a3
Hitesh Garg <y14uc106@lnmiit.ac.in>
2017-11-14 09:36:29 +0530

    events model created

--- alumni_association_website/webapp/models.py
+++ alumni_association_website/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+        STATUS_CHOICES = (
+                ('draft', 'Draft'),
+                ('live', 'Live'),
+                ('finished', 'Finished'),
+        )
+        title = models.CharField(max_length=2000, blank=False)
+        slug = models.SlugField(max_length=250, unique=True)
+        description = models.TextField()
+        datetime = models.DateTimeField(blank=False)
+        status = models.CharField(max_length=10, choices=STATUS_CHOICES,
+                                  default='draft')
