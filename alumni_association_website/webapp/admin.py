e54d94796903952bfce6bb88c777cb2c64f872b3
Hitesh Garg <y14uc106@lnmiit.ac.in>
2017-11-20 14:46:16 +0530

    completed till btp meeting 1

--- alumni_association_website/webapp/admin.py
+++ alumni_association_website/webapp/admin.py
@@ -3,10 +3,10 @@ from webapp.models import Event
 
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status')
+    list_display = ('title', 'presenter', 'presenter_position' ,'status')
     list_filter = ('status', 'datetime')
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug': ('title',)}
     ordering = ['status']
 
-admin.site.register(Event, EventAdmin)    
+admin.site.register(Event, EventAdmin)
--- alumni_association_website/webapp/models.py
+++ alumni_association_website/webapp/models.py
@@ -9,6 +9,9 @@ class Event(models.Model):
         )
         title = models.CharField(max_length=2000, blank=False)
         slug = models.SlugField(max_length=250, unique=True)
+        presenter = models.CharField(max_length=2000, blank=True)
+        presenter_position = models.CharField(max_length=2000, blank=True)
+        venue = models.CharField(max_length=2000, default='')
         description = models.TextField()
         datetime = models.DateTimeField(blank=False)
         status = models.CharField(max_length=10, choices=STATUS_CHOICES,
